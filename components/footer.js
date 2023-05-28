d57550a343f553899f0cdb0884f3d324ec55cad3
techchaze <techchaze@gmail.com>
2023-05-28 21:00:19 +0530

    updates

--- components/footer.js
+++ components/footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
       title: "Certified guides",
     },
     {
-      number: "3210",
+      number: "32",
       title: "Satisfied hikers",
     },
     {
@@ -134,14 +134,16 @@ const Footer = () => {
           width="100%"
           my="60px"
           flexWrap="wrap"
+          padding="16px"
         >
            
           {numData.map((el, id) => {
             return (
               <>
                 {visible && (
-                <Flex alignItems="center" flexDirection="column">
+                <Flex alignItems="center" flexDirection="column" justifyContent="center">
                   <Flex key={id} gap="9px" alignItems="center">
+                    <Box display={["none","none","block","block"]}>
                     <svg
                       width="56"
                       height="56"
@@ -154,16 +156,28 @@ const Footer = () => {
                         fill="#FF461D"
                       />
                     </svg>
+                    </Box>
+                    <Box display={["block","block","none","none"]} >
+                    <svg width="43" height="43" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M21.25 0.5C9.7 0.5 0.25 9.95 0.25 21.5C0.25 33.05 9.7 42.5 21.25 42.5C32.8 42.5 42.25 33.05 42.25 21.5C42.25 9.95 32.8 0.5 21.25 0.5ZM17.05 32L6.55 21.5L9.511 18.539L17.05 26.057L32.989 10.118L35.95 13.1L17.05 32Z" fill="#FF461D"/>
+</svg>
+
+                    </Box>
+                    
+
+                  
 
                     <Text
                       color="black"
-                      fontSize={["45px", "45px", "60px", "60px"]}
+                      fontSize={["30px", "45px", "60px", "60px"]}
                       fontFamily="Anton"
                       fontWeight="400"
                       lineHeight="92px"
                     >
                        <CountUp end={el?.number} delay={0}></CountUp>
                     </Text>
+                    <Box display={["none","none","block","block"]}>
+
                     <svg
                       width="25"
                       height="26"
@@ -176,9 +190,17 @@ const Footer = () => {
                         fill="#FF461D"
                       />
                     </svg>
+                    </Box>
+                    <Box display={["block","block","none","none"]}>
+
+                    <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M17.4107 10.8393H10.7143V17.5357C10.7143 17.8909 10.5732 18.2316 10.322 18.4827C10.0709 18.7339 9.7302 18.875 9.375 18.875C9.0198 18.875 8.67915 18.7339 8.42798 18.4827C8.17682 18.2316 8.03571 17.8909 8.03571 17.5357V10.8393H1.33929C0.984085 10.8393 0.643433 10.6982 0.392268 10.447C0.141103 10.1959 0 9.8552 0 9.5C0 9.1448 0.141103 8.80415 0.392268 8.55298C0.643433 8.30182 0.984085 8.16071 1.33929 8.16071H8.03571V1.46429C8.03571 1.10908 8.17682 0.768432 8.42798 0.517267C8.67915 0.266103 9.0198 0.125 9.375 0.125C9.7302 0.125 10.0709 0.266103 10.322 0.517267C10.5732 0.768432 10.7143 1.10908 10.7143 1.46429V8.16071H17.4107C17.7659 8.16071 18.1066 8.30182 18.3577 8.55298C18.6089 8.80415 18.75 9.1448 18.75 9.5C18.75 9.8552 18.6089 10.1959 18.3577 10.447C18.1066 10.6982 17.7659 10.8393 17.4107 10.8393Z" fill="#FF461D"/>
+</svg>
+
+</Box>
                   </Flex>
                   <Text
-                    fontSize={["15px", "15px", "20px", "20px"]}
+                    fontSize={["13px", "15px", "20px", "20px"]}
                     color="#111111"
                   >
                     {el.title}
